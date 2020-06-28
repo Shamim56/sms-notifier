87eb031f1d83313af6a4e27b675676b12a10b6d9
unknown <sahmed56@hotmail.com>
2020-06-27 23:51:09 -0400

    test

---
+++ src/sns.py
@@ -0,0 +1,33 @@
+import boto3, yaml
+from botocore.exceptions import ClientError
+
+
+def read_yaml():
+  """
+  reads in the config file as a map
+
+  return<Map>: a map of the configs.yml
+  """
+  data = None
+  with open('config.yml') as f:
+    data = yaml.load(f, Loader=yaml.FullLoader)
+  return data
+
+
+def main():
+    config = read_yaml()
+    client = boto3.client("sns",region_name="us-east-1")
+    try:
+        response = client.publish(
+            PhoneNumber="+15713374423",
+            Message="hwllo! This is a test message!"
+        )
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        print('sms sent!')
+        print(response)
+
+
+if __name__ == '__main__':
+    main()
